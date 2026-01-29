2b026d58f590f40890f393ee3fc6e6e7913516b3
Killnoymous <kakupro009@gmail.com>
2026-01-29 19:53:03 +0530

    Update hero image with face swap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import heroSlide1 from '../assets/hero_final_wide.png';
+import heroSlide1 from '../assets/hero_face_final.png';
 
 const slides = [
     {
